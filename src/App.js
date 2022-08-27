cb5d91396714766ead0c063b1dc490273f49dee3
Zhi Liang Ng <lawrence5682@gmail.com>
2022-08-28 04:34:05 +0800

    add swiperjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,49 @@
 import Navbar from './components/navbar/Navbar';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
 import './app.css';
+import { Navigation } from 'swiper';
+import egg from './images/slider1.png';
+import leave from './images/slider2.png';
+import prawns from './images/slider3.png';
+import lettuce from './images/slider4.png';
+import tomato from './images/slider5.png';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <div className="parallax"></div>
+      <div className="parallax-container">
+        <div className="parallax"></div>
+      </div>
+      <div className="carousel">
+        <Swiper
+          navigation={true}
+          loop={true}
+          modules={[Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img src={egg} alt="egg-img"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            {' '}
+            <img src={leave} alt="leave-img"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            {' '}
+            <img src={prawns} alt="prawn-img"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            {' '}
+            <img src={lettuce} alt="lettuce-img"></img>
+          </SwiperSlide>
+          <SwiperSlide>
+            <img src={tomato} alt="tomato-img"></img>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </div>
   );
 }
