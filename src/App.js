fe694212e950eae3f6dccd248e1834824e5408de
Zhi Liang Ng <lawrence5682@gmail.com>
2022-08-28 03:41:04 +0800

    update test

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import Navbar from './components/navbar/Navbar';
 import './app.css';
-import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Header />
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Non, molestias voluptatibus fuga perspiciatis
-        quos itaque commodi vitae illum recusandae a
-        eligendi accusantium perferendis dolorum, eaque eos
-        veniam necessitatibus facere saepe?
-      </div>
+      <div className="parallax"></div>
     </div>
   );
 }
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,18 +1,50 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './navbar.css';
 import logo from '../../images/logo.png';
 
 const Navbar = () => {
   return (
-    <div className="navbar">
-      <div className="navContainer">
-        <img src={logo} alt="Logo"></img>
-        <div className="navItems">
-          <span>About Us</span>
-          <span>Service</span>
-          <span>Contact</span>
-          <span>Career</span>
+    <div>
+      <nav class="navbar navbar-expand-md navbar-light">
+        <div class="container-fluid">
+          <img src={logo} alt="logo-img"></img>
+          <button
+            type="button"
+            class="navbar-toggler"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div
+            id="navbarCollapse"
+            class="collapse navbar-collapse"
+          >
+            <ul class="nav navbar-nav ms-auto">
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  About Us
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  Services
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  Career
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="#" class="nav-link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
+      </nav>
     </div>
   );
 };
